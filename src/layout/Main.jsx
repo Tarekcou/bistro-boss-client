f6156d0ea1ba755f93f2757b3aeceb8e3b3c44e0
Tarekcou <sitarek77@gmail.com>
2024-12-31 17:20:34 +0600

    office

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
+import Banner from "../pages/Shared/Banner/Banner";
 
 const Main = () => {
-  return <Outlet>main</Outlet>;
+  return (
+  <div>
+    <Navbar />
+  <Outlet></Outlet>;
+    <Footer />
+  </div>
+  )
+
 };
 
 export default Main;
